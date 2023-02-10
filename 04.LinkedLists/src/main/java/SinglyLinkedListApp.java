b7571aca56ffc4fbec28c1b5221c8bc073971f1b
Umran Ozman <umonal@yahoo.com>
2023-02-10 18:21:01 -0500

    SinglyLinkedListApp Class updated- isEmpty() is tested

--- 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
+++ 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -3,9 +3,8 @@ import java.util.ArrayList;
 public class SinglyLinkedListApp {
     public static void main(String[] args) {
 
+MySinglyLinkedList myList=new MySinglyLinkedList(); //Creating object to test the methods we created in MySinglyLinkedList Class
 
-
+        System.out.println(myList.isEmpty());
     }
-
-
 }
