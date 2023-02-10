9f1fbb60adf554a067671a0da3ffb09b052fbf15
Umran Ozman <umonal@yahoo.com>
2023-02-10 16:59:53 -0500

    Node Class created

---
+++ 04.LinkedLists/src/main/java/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+   int id;      // This is my data that I want to store in my node
+   Node next;   // This is the address of the next node stored in my current node
+
+
+    public Node (int id, Node next) {  //If we want to use this node and these fields we need a constructor.
+        this.id = id;
+        this.next = next;
+    }
+}
+
+
