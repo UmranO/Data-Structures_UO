a469836aa0002902741380f75ed70489b47f57fa
Umran Ozman <umonal@yahoo.com>
2023-02-10 18:14:29 -0500

    MySinglyLinkedList Class created

---
+++ 04.LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class MySinglyLinkedList {
+
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){       //if head is null then it'll return true which means we don't have any elem.in the Singly Linked List
+        return head==null;
+    }
+
+
+
+}
