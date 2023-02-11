c54ef6ab80acf06854769aec58a0846123270d61
Umran Ozman <umonal@yahoo.com>
2023-02-10 21:59:24 -0500

    MySinglyLinkedList Class updated- add()

--- 04.LinkedLists/src/main/java/MySinglyLinkedList.java
+++ 04.LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -6,10 +6,28 @@ public class MySinglyLinkedList {
     Node tail;
     int size;
 
-    boolean isEmpty(){       //if head is null then it'll return true which means we don't have any elem.in the Singly Linked List
-        return head==null;
+    boolean isEmpty() {       //if head is null then it'll return true which means we don't have any elem.in the Singly Linked List
+        return head == null;
+    }
+
+    void add(int data) {                  //We are sending an integer value
+        Node node = new Node(data);       //Created a new node object from the sent int data
+
+        //There are 2 cases: 1)List is empty 2)List is not empty
+
+        if (isEmpty()) {                  // if the List is empty
+            head = tail = node;
+            size++;
+
+        } else {                          // if the List is not empty
+
+            tail.next = node;             //Assign node to tail.next
+            tail = node;                  //Since we added to the end we have to define our new tail which is node.
+            size++;
+        }
     }
 
 
+    }
 
-}
+
