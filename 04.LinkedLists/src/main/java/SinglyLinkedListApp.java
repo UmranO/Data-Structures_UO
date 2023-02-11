2cea6fa005ed8250f5d29b451c42ebfee573d53e
Umran Ozman <umonal@yahoo.com>
2023-02-10 22:01:12 -0500

    SinglyLinkedListApp Class updated - printNodes() tested

--- 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
+++ 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -3,8 +3,15 @@ import java.util.ArrayList;
 public class SinglyLinkedListApp {
     public static void main(String[] args) {
 
-MySinglyLinkedList myList=new MySinglyLinkedList(); //Creating object to test the methods we created in MySinglyLinkedList Class
+    MySinglyLinkedList myList=new MySinglyLinkedList();       //Creating object to test the methods we created in
+                                                              // MySinglyLinkedList Class
+    System.out.println(myList.isEmpty());
 
-        System.out.println(myList.isEmpty());
+        for (int i = 0; i < 10; i++) {                        //Made a fori loop and added items into the list.
+            myList.add(i);
+        }
+        myList.printNodes();                                  //to check if they are added correctly called printNodes()
+
+
     }
 }
