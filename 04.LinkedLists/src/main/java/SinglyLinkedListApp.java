3fa17f2024df33187583afdc912f02a1818cc502
Umran Ozman <umonal@yahoo.com>
2023-02-11 18:20:39 -0500

    SinglyLinkedListApp Class updated

--- 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
+++ 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-
 public class SinglyLinkedListApp {
+
     public static void main(String[] args) {
 
     MySinglyLinkedList myList=new MySinglyLinkedList();       //Creating object to test the methods we created in
