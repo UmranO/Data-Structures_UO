317169c6ce3ab9b65f6eebe2acf9ab2f5ca8a5dd
Umran Ozman <umonal@yahoo.com>
2023-02-11 20:40:16 -0500

    UOSinglyLinkedTest Class created

--- 04.LinkedLists/src/main/java/UOSinglyLinkedTest.java
+++ 04.LinkedLists/src/main/java/UOSinglyLinkedTest.java
@@ -1,12 +1,23 @@
 public class UOSinglyLinkedTest {
+
     public static void main(String[] args) {
+
         UOSinglyLinkedList uList=new UOSinglyLinkedList();
+
+        System.out.println(uList.isEmpty());
 
         for (int i = 0; i < 10; i++) {
             uList.add(i);
         }
+        uList.printNodes();
 
-       uList.printNodes();
+        uList.deleteById(9);
+        uList.printNodes();
 
+        uList.deleteById(4);
+        uList.printNodes();
+
+        uList.deleteById(0);
+        uList.printNodes();
     }
 }
