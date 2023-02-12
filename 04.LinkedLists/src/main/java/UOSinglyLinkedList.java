6c427f381df30350bf32baad4d31e3db775530f7
Umran Ozman <umonal@yahoo.com>
2023-02-11 20:38:20 -0500

    UOSinglyLinkedList Class created

--- 04.LinkedLists/src/main/java/UOSinglyLinkedList.java
+++ 04.LinkedLists/src/main/java/UOSinglyLinkedList.java
@@ -25,22 +25,53 @@ public class UOSinglyLinkedList {
             }
         }
 
-        //----PrintNodes Method------------------------------------------------------------------------------------------------
+ //----PrintNodes Method------------------------------------------------------------------------------------------------
 
         void printNodes() {
 
             Node current = head;
+
             while (current != null) {
-                if (current.next == null) System.out.println(current.id + " => null");
-                else {
-                    System.out.print(current.id + " => ");
-                }
+
+               if (current.next == null) System.out.println(current.id + " => null");
+                else {System.out.print(current.id + " => ");}
                 current = current.next;
             }
         }
+//----Delete Method------------------------------------------------------------------------------------------------
+        void deleteById (int id){
+         //Check if the list is empty
+            if(isEmpty()){
+                System.out.println("List is empty");
+                     }
+         //Assign prev and current with the head to reset your pointers to the beginning
+            Node prev=head;
+            Node current=head;
+         while(current!=null){
+             if (current.id==id) {
+                 //if it is the Head
+                 if (current == head) {
+                     head = current.next;
+                     current.next = null;
+                 }
+                 //if it is the Tail
+                 else if (current == tail) {
+                     tail = prev;
+                     prev.next = null;
+                 }
 
+                 // if it is in the middle
+                 else {
+                     prev.next = current.next;
+                     current.next=null;
+                 }size--;
+             }
+         prev=current;
+         current=current.next;
 
-    }
+         }
+
+        }}
 
 /*
 -Create isEmpty()
