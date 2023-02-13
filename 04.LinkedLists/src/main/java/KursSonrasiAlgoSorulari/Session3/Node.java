b859f85e2e3d952e9ce915d8f7f6b22e55e0c5d4
Umran Ozman <umonal@yahoo.com>
2023-02-13 12:22:33 -0500

    Session3 Node Class created

---
+++ 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/Node.java
@@ -0,0 +1,21 @@
+package KursSonrasiAlgoSorulari.Session3;
+
+public class Node {
+    Node next;
+    String name;
+    String lastName;
+    String email;
+    String phoneNumber;
+
+    public Node(String name, String lastName, String email, String phoneNumber) {
+
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+
+    }
+}
+// We created a Node Class & with this Node class we can point to other Node Classes. And we have some Data like name,
+//email.....
+//We don't include Node next in the Constructor bec. we want it to be null by default.
