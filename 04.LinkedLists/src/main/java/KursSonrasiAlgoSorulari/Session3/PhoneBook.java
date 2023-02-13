3ec9c611606f18a0ee28b523a836db4e4007a234
Umran Ozman <umonal@yahoo.com>
2023-02-13 12:23:45 -0500

    PhoneBook Class created

---
+++ 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/PhoneBook.java
@@ -0,0 +1,14 @@
+package KursSonrasiAlgoSorulari.Session3;
+
+public class PhoneBook {
+    Node head;
+    Node tail;
+    int size;
+
+    public PhoneBook() {
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+}
