e3551c36c9682856b968bdbb42e26938ba746cce
Umran Ozman <umonal@yahoo.com>
2023-02-13 12:34:51 -0500

    PhoneBookTest Class created /size() tested

--- 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/PhoneBookTest.java
+++ 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/PhoneBookTest.java
@@ -10,11 +10,18 @@ public class PhoneBookTest {
         mylist.insert("Un","Oan","um@yahoo.com","678");
         mylist.insert("Oz","l","P@gmail.com","153");
 
+//---- Test size ()---------------------------------------------------------------------------------------------------
+
+        System.out.println(mylist.size);
 
 //---- Test isEmpty ()------------------------------------------------------------------------------------------------
 
         System.out.println(mylist.isEmpty());
 
+//---- Test indexOf ()------------------------------------------------------------------------------------------------
+
+        System.out.println(mylist.indexOf("o@yahoo.com"));
+        System.out.println(mylist.indexOf("u@yahoo.com"));
 
     }
 }
