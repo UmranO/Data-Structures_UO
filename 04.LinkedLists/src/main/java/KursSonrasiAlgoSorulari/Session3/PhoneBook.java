c6e600ea3b81b47daa8bf3ecf2010dd2f09f9664
Umran Ozman <umonal@yahoo.com>
2023-02-13 15:02:24 -0500

    PhoneBook Class updated /findByName () added

--- 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/PhoneBook.java
+++ 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/PhoneBook.java
@@ -13,11 +13,11 @@ public class PhoneBook {
 //Now we 've created the Phonebook so what we need is: we need an insert(), size(), indexOf(), findByName(),
 // deleteByEmail(), sortByName(), A () for deleting the duplicated entries
 
-    //----isEmpty Method----------------------------------------------------------------------------------------------------
+    //----isEmpty Method------------------------------------------------------------------------------------------------
     boolean isEmpty(){
         return  head==null;
     }
-    //----Add Method-(Adds to the end)--------------------------------------------------------------------------------------
+    //----Add Method-(Adds to the end)----------------------------------------------------------------------------------
     void insert(String name, String lastName, String email, String phoneNumber) {//we're sending them as parameters &
         // it will create a node inside the method & it will add that node into the linked list
         //Now to be on the safe side what is recommended is put the main steps
@@ -28,14 +28,14 @@ public class PhoneBook {
             head = tail = node;
         }           //if list is empty the new node will be the head and the tail
         //it's the only node in the list
-        else {                          //if this is not the case: if the list is not empty you continue with the else part
-            tail.next = node;           //we'll jump into the last one add it after the tail
-            tail=node;                  //new node is assigned to the tail. Now new node is the tail
-        }                               //By default node.next is null
+        else {                       //if this is not the case: if the list is not empty you continue with the else part
+            tail.next = node;        //we'll jump into the last one add it after the tail
+            tail=node;               //new node is assigned to the tail. Now new node is the tail
+        }                            //By default node.next is null
 
         size++;                     //after the if & else part before getting out of the () size should be increased
     }
-    //----Size Method-(Adds to the end)-------------------------------------------------------------------------------------
+    //----Size Method-(Adds to the end)---------------------------------------------------------------------------------
     /* if the variable size is given as above the size method is:
     int size () {return size;}
     BUT if the int size is not given we have to begin from the head & go through
@@ -57,8 +57,8 @@ public class PhoneBook {
         }
         return size;
     }
-    //----IndexOf Method----------------------------------------------------------------------------------------------------
-    int indexOf(String email){                         // send an email as a parameter and return the index of it (an int #)
+    //----IndexOf Method------------------------------------------------------------------------------------------------
+    int indexOf(String email){                     // send an email as a parameter and return the index of it (an int #)
 
         Node current=head;
         int index=0;
@@ -77,7 +77,28 @@ public class PhoneBook {
         }
 
     }
+//----FindByName Method-------------------------------------------------------------------------------------------------
+    Node findByName(String name) {
+        Node current;
+        current = head;
+        Node next;
+        while (current != null) {
+            if (current.name == name) {
+                return current;
+            }
+            current = current.next;
 
+        }
+        return null;
+    }
+
+//----DeleteByEmail Method----------------------------------------------------------------------------------------------
+//----SortByName Method-------------------------------------------------------------------------------------------------
+//----DeleteDuplicatedEntry Method--------------------------------------------------------------------------------------
+
+
+
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 //We'll create PhoneBook Class & this PhoneBook Class will hold nodes.
@@ -94,5 +115,5 @@ public class PhoneBook {
 
 
 
-}
+
 
