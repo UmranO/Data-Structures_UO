a8a4f6beda9441b2e8348dcf9195f35176347aa7
Umran Ozman <umonal@yahoo.com>
2023-02-14 13:43:32 -0500

    PhoneBookTest Class updated /toString(),sortByNameSwap () tested created

--- 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/PhoneBookTest.java
+++ 04.LinkedLists/src/main/java/KursSonrasiAlgoSorulari/Session3/PhoneBookTest.java
@@ -5,16 +5,23 @@ public class PhoneBookTest {
     public static void main (String[] args){
         PhoneBook mylist=new PhoneBook();
 
+        PhoneBook liste=new PhoneBook();
+
 System.out.println("---- Test insert ()-------------------------------------------------------------------------");
 
 
         mylist.insert("Cn","Oan","b@yahoo.com-0","678");
         mylist.insert("Eal","m","cgmail.com-1","100");
         mylist.insert("Amran","Ozman","a@yahoo.com","345");      //deleted
-        mylist.insert("Kum","w","Poioooo@gmail.com","445153");
+        mylist.insert("Kum","w","Poioooo@gmail.com","445153");   //deleted
         mylist.insert("y","u","yll@gmail.com-3","3");
         mylist.insert("Mete","Om","f@H-2","123456");
         mylist.insert("z","l","z@gmail.com-4","153");
+
+        liste.insert("a","l","index of a","153");         //index of a 0
+        liste.insert("c","l","index of c","153");         //index of c 1
+        liste.insert("b","l","index of b","153");         //index of b 2
+        liste.insert("t","l","index of t","245");         //index of t 3
 
 System.out.println("---- Test size ()-------------------------------------------------------------------------");
 
@@ -48,16 +55,15 @@ System.out.println("----Test DeleteByEmail ()-----------------------------------
 
         System.out.println(mylist.indexOf("Poioooo@gmail.com"));
 
-        System.out.println("----Test SortByName ()-------------------------------------------------------------------");
+System.out.println("----Test SortByNameSwap----------------------------------------------------------------");
+
+          mylist.sortByNameSwap();
+          System.out.println("--sortByName liste:");
+          System.out.println(mylist);
+          liste.sortByNameSwap();
+          System.out.println(liste);
 
 
-
-
-
-
-
-
- System.out.println("----Test DeleteDuplicates ()-------------------------------------------------------------");
 
 }
 }
